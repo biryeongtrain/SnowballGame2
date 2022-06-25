37cb00cea1ee2da103a9b9005ba524ac4876ae31
tattertot21 <108112272+tattertot21@users.noreply.github.com>
2022-06-24 22:07:36 -0400

    Update SnowballGameAPI.java

--- src/main/java/com/github/kanesada2/SnowballGame/api/SnowballGameAPI.java
+++ src/main/java/com/github/kanesada2/SnowballGame/api/SnowballGameAPI.java
@@ -55,7 +55,7 @@ public class SnowballGameAPI {
 		}
 		if(isPitching){
 			Bukkit.getPluginManager().callEvent(new BallThrownEvent(launched));
-			Collection<Entity> entities = launched.getNearbyEntities(50, 10, 50);
+			Collection<Entity> entities = launched.getNearbyEntities(30, 10, 30);
 			for (Entity entity : entities) {
 				if(entity instanceof ArmorStand && Util.isUmpire(((ArmorStand)entity).getBoots())){
 					Location inBottom = entity.getLocation().add(new Vector(-0.5, plugin.getConfig().getDouble("Umpire.Bottom"), -0.5));
